d4781077ed9c5da71f80e645023e8252d1f5cb0d
tahoeRobbo <rob@snowyplayground.com>
2015-07-03 12:48:15 -0600

    added dummy html for workflow

--- exercise1_5.js
+++ exercise1_5.js
@@ -24,4 +24,26 @@ var idf = function(x) {
 var test = idf('x');
 console.log(test()); // x
 
-//
+//4- Write a function that adds from two invocations
+// addf(3)(4); //7
+
+var addf = function(x) {
+	return function(y) {
+		return x + y;
+	};
+};
+
+console.log(addf(3)(4));
+
+//5- write a function that takes in a binary function and is
+//callable from two invocations
+
+var superFunc= function(binaryFunc) {
+	return function(x) {
+		return function(y) {
+			return binaryFunc(x, y);
+		};
+	};
+};
+
+console.log(superFunc(mul)(4)(5));//20
